e18d69969646595806f2d4cee2b3b47817281ada
Mohammed Ramadan <mohammed.ramadan1474@gmail.com>
2023-01-01 19:40:02 +0200

    adds delete solution logic

--- src/app/components/solution-list/solution-list.component.ts
+++ src/app/components/solution-list/solution-list.component.ts
@@ -1,5 +1,7 @@
 import { Solution } from '../../interfaces/solution';
 import { Component, Input } from '@angular/core';
+import { SolutionService } from 'src/app/services/solution.service';
+import { findIndex } from 'rxjs';
 
 @Component({
   selector: 'app-solution-list',
@@ -9,4 +11,17 @@ import { Component, Input } from '@angular/core';
 export class SolutionListComponent {
   @Input() solutions: Solution[] = [];
   @Input() userEmail: string = '';
+
+  constructor(private solutionService: SolutionService) {}
+
+  deleteSolution(solutionId: number) {
+    console.table(this.solutions);
+    this.solutionService.deleteUserSolution(solutionId).subscribe(() => {
+      const solutionIndex = this.solutions.findIndex(
+        (solution) => solution.solution_id === solutionId
+      );
+      this.solutions.splice(solutionIndex, 1);
+      console.table(this.solutions);
+    });
+  }
 }
